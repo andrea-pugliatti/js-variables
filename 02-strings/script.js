980ef230a95a87fa6e33775f2c2686727e6630c0
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-03 13:05:09 +0200

    Add second exercise in 02-strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -9,6 +9,10 @@ console.log(name);
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number = 10;
+const text = "Questa è una stringa";
+const bool = true;
+console.log(number, text, bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 

4d416bc3bc28a65b94f9ceab1efccbb2b55e8ac9
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-10-03 15:18:27 +0200

    Fix exercise 2 of 03-string-bonus

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -11,7 +11,7 @@ console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = city.split(' ').at(1); // oppure city.split(' ')[1]
+const citySubstring = city.substring(4);
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
